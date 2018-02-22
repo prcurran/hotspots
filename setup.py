0be8edbda021c41a40dd6efe857044e34e7b1aae
prcurran <pc564@cam.ac.uk>
2018-02-22 12:13:50 +0000

    setup

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Fragment Hotspot Maps",
+    name="fragment-hotspot-maps",
     version="0.1",
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
-    scripts=['src/run_hotspot.py'],
+    #scripts=['src/run_hotspot.py'],
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires=['numpy', 'csd-python-api', 'ccdc_internal', 'matplotlib', 'scipy', 'sklearn'],
+    install_requires=['numpy', 'csd-python-api', 'matplotlib', 'scipy', 'sklearn'],
 
     package_data={
         # If any package contains *.txt or *.rst files, include them:
